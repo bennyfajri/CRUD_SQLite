3281d25147d5d84f284f664d917f4d8fc3541170
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2021-11-10 15:03:01 +0700

    Show all data on recyclerview

--- app/src/main/java/com/example/crud_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/crud_sqlite/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.crud_sqlite
 
 import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.crud_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var myDB: MyDatabaseHelper
+    lateinit var book_id: ArrayList<String>
+    lateinit var book_title:ArrayList<String>
+    lateinit var book_author:ArrayList<String>
+    lateinit var book_page:ArrayList<String>
+    lateinit var customAdapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,33 @@ class MainActivity : AppCompatActivity() {
         binding.fabAdd.setOnClickListener {
             val intent = Intent(applicationContext, AddActivity::class.java)
             startActivity(intent)
+        }
+
+        myDB = MyDatabaseHelper(this@MainActivity)
+        book_id = ArrayList()
+        book_title = ArrayList()
+        book_author = ArrayList()
+        book_page = ArrayList()
+
+        storeDataInArrays()
+
+        customAdapter = CustomAdapter(this@MainActivity)
+        customAdapter.setData(book_id, book_title, book_author, book_page)
+        binding.recyclerView.adapter = customAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+    fun storeDataInArrays() {
+        val cursor: Cursor? = myDB.readAllData()
+        if(cursor?.count == 0){
+            Toast.makeText(this@MainActivity, "No data.", Toast.LENGTH_SHORT).show()
+        }else{
+            while (cursor!!.moveToNext()){
+                book_id.add(cursor.getString(0))
+                book_title.add(cursor.getString(1))
+                book_author.add(cursor.getString(2))
+                book_page.add(cursor.getString(3))
+            }
         }
     }
 }
